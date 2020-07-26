89cbdadc6de0f4eadd0afb4c7820c14a81a659e1
Yury Petrashev <yurapetr@yahoo.com>
2020-07-26 18:51:05 +0700

    Added FAB on main activity

--- app/src/main/java/com/example/android/tp_helper/MainActivity.java
+++ app/src/main/java/com/example/android/tp_helper/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.android.tp_helper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab_add_article);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
