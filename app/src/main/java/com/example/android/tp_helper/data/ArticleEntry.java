278422c89abca27d6f4bd07017eeee94de1594c1
Yury Petrashev <yurapetr@yahoo.com>
2020-07-26 14:57:38 +0700

    Create ArticleEntry

---
+++ app/src/main/java/com/example/android/tp_helper/data/ArticleEntry.java
@@ -0,0 +1,43 @@
+package com.example.android.tp_helper.data;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "article")
+public class ArticleEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String content;
+
+    public ArticleEntry(int id, String name, String content){
+        this.id = id;
+        this.name = name;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
