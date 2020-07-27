62b0b7ac014b7a0b8cdde2d3762504430d87967d
Yury Petrashev <yurapetr@yahoo.com>
2020-07-27 18:19:14 +0700

    Implement showing articles in main activity

---
+++ app/src/main/java/com/example/android/tp_helper/ArticlesAdapter.java
@@ -0,0 +1,109 @@
+package com.example.android.tp_helper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ArticleViewHolder> {
+
+    private JSONArray mArticlesData = new JSONArray();
+
+    private final ArticleAdapterOnClickHandler mClickHandler;
+
+    public interface ArticleAdapterOnClickHandler{
+        void onClick(JSONObject articleItem);
+    }
+
+    public ArticlesAdapter(ArticleAdapterOnClickHandler clickHandler){
+        mClickHandler = clickHandler;
+    }
+
+    @NonNull
+    @Override
+    public ArticleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.article_item_in_main;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldBeAttachToParent = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldBeAttachToParent);
+        ArticleViewHolder viewHolder = new ArticleViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ArticleViewHolder articleViewHolder, int position) {
+        String currentArticleName = null;
+        try {
+            JSONObject currentArticle = (JSONObject) mArticlesData.get(position);
+            currentArticleName = currentArticle.getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        articleViewHolder.articleItemTextView.setText(currentArticleName);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mArticlesData == null){return 0;}
+
+        return mArticlesData.length();
+    }
+
+    public class ArticleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+//        ImageView articleItemImageView;1
+        TextView articleItemTextView;
+
+        public ArticleViewHolder(View itemView) {
+            super(itemView);
+            articleItemTextView = itemView.findViewById(R.id.tv_article_block_item);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int adapterPosition = getAdapterPosition();
+            JSONObject articleItem = null;
+            try {
+                articleItem = (JSONObject) mArticlesData.get(adapterPosition);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mClickHandler.onClick(articleItem);
+        }
+    }
+
+    public void setArticlesData(JSONArray articlesData){
+        int arraySize = articlesData.length();
+        for(int i = 0; i < arraySize; i++){
+            try {
+                JSONObject currentJsonItem = (JSONObject) articlesData.getJSONObject(i);
+                mArticlesData.put(currentJsonItem);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public void clearData(){
+        while(mArticlesData.length() > 0){
+            mArticlesData.remove(0);
+        }
+    }
+}
--- app/src/main/java/com/example/android/tp_helper/MainActivity.java
+++ app/src/main/java/com/example/android/tp_helper/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.android.tp_helper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.android.tp_helper.data.AppDatabase;
+import com.example.android.tp_helper.data.ArticleEntry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ArticlesAdapter.ArticleAdapterOnClickHandler{
 
     FloatingActionButton fab;
+    ArticlesAdapter mAdapter;
+    RecyclerView mRecyclerView;
+    private AppDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +41,65 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mRecyclerView = findViewById(R.id.rv_articles_list);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(manager);
+        mAdapter = new ArticlesAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onClick(JSONObject articleItem) {
+        Toast.makeText(this, "CLicked item", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        mAdapter.clearData();
+        mDb = AppDatabase.getInstance(getApplicationContext());
+        loadArticlesData();
+        super.onResume();
+    }
+
+    private class FetchAllArticlesTask extends AsyncTask<Void, Void, List<ArticleEntry>> {
+        @Override
+        protected List<ArticleEntry> doInBackground(Void... voids) {
+            mDb = AppDatabase.getInstance(getApplicationContext());
+            final JSONArray[] articleJsonData = null;
+            final List<ArticleEntry> articleEntries = mDb.articleDao().loadAllArticles();
+
+            return articleEntries;
+        }
+
+        @Override
+        protected void onPostExecute(List<ArticleEntry> articleEntries) {
+            if(articleEntries.size() > 0){
+                JSONArray jsonArray = new JSONArray();
+                int sizeOfList = articleEntries.size();
+
+                for(int i = 0; i < sizeOfList; i++){
+                    String name = articleEntries.get(i).getName();
+                    String content = articleEntries.get(i).getContent();
+                    JSONObject jsonObject= null;
+                    try {
+                        jsonObject = new JSONObject()
+                                .put("name", name)
+                                .put("content", content);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    jsonArray.put(jsonObject);
+                }
+
+                mAdapter.setArticlesData(jsonArray);
+            }
+        }
+    }
+
+    private void loadArticlesData(){
+        new FetchAllArticlesTask().execute();
     }
 }
